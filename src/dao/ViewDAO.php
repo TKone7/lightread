630b8e61c0ba6a6d4a93c0535d3d59039d41987e
RomanBoegli <roman-boegli@hotmail.com>
2019-11-26 22:23:15 +0100

    non-users' views are now counted correctly

--- src/dao/ViewDAO.php
+++ src/dao/ViewDAO.php
@@ -12,27 +12,25 @@ class ViewDAO extends BasicDAO
 
     public function register(View $view) {
         $stmt = $this->pdoInstance->prepare('
-        INSERT INTO tbl_views (fld_user_id, fld_cont_id, fld_view_sessionid, fld_view_ip, fld_view_city, fld_view_country, 
-                               fld_view_os, fld_view_browser, fld_view_screensize, fld_view_searcheng, fld_view_pit)
-          values(:user_id, :cont_id, :sessionid, :ip, :city, :country, :os, :browser, :screensize, :searcheng, :pit)');
+        INSERT INTO tbl_views (fld_user_id, fld_cont_id, fld_view_ip, fld_view_country, fld_view_city, 
+                               fld_view_os, fld_view_browser, fld_view_browservrs, fld_view_pit)
+          values(:user_id, :cont_id, :ip,  :country, :city, :os, :browser, :browservrs, :pit)');
         $u = !is_null($view->getUser()->getId()) ? $view->getUser()->getId() : NULL;
-        $stmt->bindValue(':user_id', $u);
+        $stmt->bindParam(':user_id', $u);
         $stmt->bindValue(':cont_id', $view->getContent()->getId());
-        $stmt->bindValue(':sessionid', $view->getSession());
         $stmt->bindValue(':ip', $view->getIp());
-        $stmt->bindValue(':city', $view->getCity());
         $stmt->bindValue(':country', $view->getCountry());
+        $stmt->bindValue(':city', $view->getCity());
         $stmt->bindValue(':os', $view->getOs());
         $stmt->bindValue(':browser', $view->getBrowser());
-        $stmt->bindValue(':screensize', $view->getScreensize());
-        $stmt->bindValue(':searcheng', $view->getSearcheng());
+        $stmt->bindValue(':browservrs', $view->getBrowserVersion());
         date_default_timezone_set('Europe/Zurich');
         $stmt->bindValue(':pit', $timestamp = date('Y-m-d H:i:s'));
         $stmt->execute();
     }
 
 
-    public function readLast(User $user, Content $content, $ip, $city, $country, $os, $browser) {
+    public function readLast(User $user, Content $content, $ip, $city, $country, $os, $browser, $browserversion) {
 
         if(!is_null($user->getId())) {
             //check by user_id
@@ -45,23 +43,20 @@ class ViewDAO extends BasicDAO
         } else {
             //check by spied data
             $stmt = $this->pdoInstance->prepare('
-                SELECT MAX(fld_view_pit) as view_pit FROM tbl_views 
-                WHERE fld_view_ip = :ip AND fld_view_city = :city AND fld_view_country = :country AND 
-                      fld_view_os = :os AND fld_view_browser = :browser;');
-            $stmt->bindValue(':ip', $ip);
-            $stmt->bindValue(':city', $city);
-            $stmt->bindValue(':country', $country);
-            $stmt->bindValue(':os', $os);
-            $stmt->bindValue(':browser', $browser);
+                SELECT MAX(fld_view_pit) as view_pit FROM tbl_views
+                WHERE fld_cont_id = :cont_id AND fld_view_ip = :ip AND fld_view_city = :city AND
+                      fld_view_country = :country AND fld_view_os = :os AND fld_view_browser = :browser AND fld_view_browservrs = :browservrs;');
+            $stmt->bindValue(':cont_id', $content->getId());
+            $stmt->bindParam(':ip', $ip);
+            $stmt->bindParam(':country', $country);
+            $stmt->bindParam(':city', $city);
+            $stmt->bindParam(':os', $os);
+            $stmt->bindParam(':browser', $browser);
+            $stmt->bindParam(':browservrs', $browserversion);
         }
 
         $stmt->execute();
         $pit = $stmt->fetchAll(\PDO::FETCH_ASSOC)[0]['view_pit'];
-
-        ///MAX always returns an record
-        //if ($stmt->rowCount() > 0) {
-        //    return $stmt->fetchAll(\PDO::FETCH_ASSOC)[0]
-        //}
 
         return $pit;
     }
