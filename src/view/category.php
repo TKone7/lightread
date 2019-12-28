6ee13abbaa948df4fc57eb842bd651f728793355
RomanBoegli <roman-boegli@hotmail.com>
2019-12-28 13:51:48 +0100

    optimized responsiveness in /Category

--- src/view/category.php
+++ src/view/category.php
@@ -15,32 +15,39 @@ $cols = 2;
 
 
 <div class="container">
+    <div class="row">
+        <div class="col-md-10 col-lg-8 mx-auto">
+            <section class="cms-boxes">
+                <div class="container-fluid">
+                    <?php
+                    $rowcount = 0;
+                    foreach ($categories as $cat) { ?>
 
-    <?php
-    $rowcount = 0;
-    foreach ($categories as $cat) { ?>
+                        <?php if($rowcount % $cols == 0){ ?>
+                            <div class="row">
+                        <?php } ?>
 
-        <?php if($rowcount % $cols == 0){ ?>
-            <div class="row category_row">
-        <?php } ?>
-
-                <div class="col category_col">
-                    <div class="category_container">
-                        <a href="<?php echo $GLOBALS["ROOT_URL"] .'/'. Router::getInstance()->route('article_category', [$cat->getKey()]);?>">
-                            <img class="category_img" src='assets/img/<?php echo $cat->getKey()?>.jpg' )>
-                            <div class="category_name">
-                                <h1 class="h1 category_h1"><?php echo $cat->getName(); ?></h1>
+                            <div class="col-md-6 cms-boxes-outer">
+                                <a href="<?php echo $GLOBALS["ROOT_URL"] .'/'. Router::getInstance()->route('article_category', [$cat->getKey()]);?>">
+                                    <div class="cms-boxes-items cms-pink" style="background-image:url('assets/img/<?php echo $cat->getKey()?>.jpg');">
+                                        <div class="boxes-align">
+                                            <div class="large-box">
+                                                <h3 style="color:#000000; background-color:rgba(255,255,255,0.26)"> <?php echo $cat->getName(); ?></h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
-                        </a>
-                    </div>
+
+                        <?php if(($rowcount +1) % $cols == 0){ ?>
+                            </div>
+                        <?php } ?>
+                        <?php $rowcount++  ?>
+                    <?php } ?>
                 </div>
-
-        <?php if(($rowcount +1) % $cols == 0){ ?>
-            </div>
-        <?php } ?>
-        <?php $rowcount++  ?>
-    <?php } ?>
-
+            </section>
+        </div>
+    </div>
 </div>
 
 
